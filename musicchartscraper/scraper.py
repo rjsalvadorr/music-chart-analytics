1999054d87d20d6005ad100b291d9dd87a466b89
RJ Salvador <randolph.salvador@gmail.com>
2017-05-26 23:01:14 -0400

    Tab content can now be retrieved by the scraper. Updated gitignore

--- musicchartscraper/scraper.py
+++ musicchartscraper/scraper.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://tabs.ultimate-guitar.com/m/marvin_gaye/whats_going_on_ver2_crd.htm")
-print("Status: " + str(page.status_code))
+resp = requests.get("https://tabs.ultimate-guitar.com/m/marvin_gaye/whats_going_on_ver3_crd.htm")
+
+print("Status: " + str(resp.status_code))
+
+pageContent = resp.content
+soup = BeautifulSoup(pageContent, "html.parser")
+
+tabContentHtml = soup.select(".js-tab-content")[0]
+tabContent = tabContentHtml.get_text()
+
+print(tabContent)
