78e4ee2316a505a2e170ab9621479ea36f80f4b0
RJ Salvador <rjsalvadorr@gmail.com>
2017-05-31 18:44:38 -0400

    Created logger class.

---
+++ musicchartscraper/logger.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import traceback
+from datetime import datetime
+
+class Logger:
+    def __init__(self):
+        self.moduleDir = os.path.dirname(os.path.realpath(__file__))
+        self.projectDir = os.path.dirname(self.moduleDir)
+        self.outputDir = os.path.join(self.projectDir, "mchartanalyzer-output")
+
+        # Create output directory if it doesn't exist
+        try:
+            os.makedirs(self.outputDir)
+        except OSError:
+            if not os.path.isdir(self.outputDir):
+                raise
+
+        self.newFile()
+
+
+    def newFile(self):
+        """
+        Starts a new log file.
+        """
+        self.startTime = datetime.now()
+        self.filename = "log-" + self.startTime.strftime("%Y%m%d-%H%M%S") + ".txt"
+        self.logPath = os.path.join(self.outputDir, self.filename)
+
+        self.log("LOG STARTED AT " + self.startTime.strftime("%Y-%m-%d %H:%M:%S"))
+        self.log("==================================\n")
+
+
+    def log(self, text):
+        try:
+            with open(self.logPath, 'a') as outFile:
+                outFile.write(text)
+                outFile.write("\n")
+        except IOError as exc:
+            print("ERROR! Unable to copy file. " + repr(exc))
+        except Exception as exc:
+            print("Unexpected error: " + repr(exc))
+            print(traceback.format_exc())
--- musicchartscraper/musicchartscraper.py
+++ musicchartscraper/musicchartscraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import requests
 from bs4 import BeautifulSoup
+from logger import Logger
 
 """
 Steps:
@@ -12,12 +13,18 @@ Potential filename format: artist-name_song-name_session-id.md
 
 class MusicChartScraper:
     def __init__(self):
+        self.logger = Logger()
         self.chordSheetLinks = []
+
+    def log(self, text):
+        self.logger.log(text)
 
     def scrape(self, artistName):
         """
         Scrapes websites for songs by a given artist.
         """
+
+        self.log("Example Text")
 
         # TODO - use strategies to get list of URLs for a specific artist on a specific page.
 
@@ -25,7 +32,7 @@ class MusicChartScraper:
 
         for sheetLink in self.chordSheetLinks:
             resp = requests.get(sheetLink)
-            print("Status: " + str(resp.status_code))
+            self.log("Status: " + str(resp.status_code))
 
             pageContent = resp.content
             soup = BeautifulSoup(pageContent, "html.parser")
@@ -33,7 +40,7 @@ class MusicChartScraper:
             tabContentHtml = soup.select(".js-tab-content")[0]
             tabContent = tabContentHtml.get_text()
 
-            print(tabContent)
+            self.log(tabContent)
 
             # TODO - run parser on each sheet, and extract features.
 
@@ -41,3 +48,5 @@ if __name__ == '__main__':
     # main method. This is where you're going to call the scraper.
     mcScraper = MusicChartScraper()
     mcScraper.scrape("placeholderValue")
+
+    print("Scraping complete!\n")
