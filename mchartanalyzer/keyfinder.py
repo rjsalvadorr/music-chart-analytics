6a7da06dbf978d8741875df8e305219a7388df1c
RJ Salvador <rjsalvadorr@gmail.com>
2017-10-05 18:54:38 -0400

    Added files for key finder functionality

---
+++ mchartanalyzer/keyfinder.py
@@ -0,0 +1,14 @@
+
+class KeyFinder:
+    """
+    Analyzes chord sequences for keys and other data
+    """
+
+    def __init__(self):
+        pass
+
+    def analyzeKey(self, chordSequence):
+        """
+        From the given chordSequence, analyze the key (or keys).
+        """
+        pass
