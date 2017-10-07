23d966644e4669420b46ef3494620ca77db3049d
RJ Salvador <rjsalvadorr@gmail.com>
2017-10-07 14:01:17 -0400

    Fleshed out chord progression detectors and test cases!

--- mchartanalyzer/keyfinder.py
+++ mchartanalyzer/keyfinder.py
@@ -1,9 +1,8 @@
 from decimal import *
 
-from music21 import harmony
-from music21 import key
+from music21 import harmony, key, chord, roman
 
-from utils import Utils
+from utils import *
 
 class KeyFinder:
     """
@@ -15,8 +14,61 @@ class KeyFinder:
         self.keyListMinor = ['e-', 'b-', 'f', 'c', 'g', 'd', 'a', 'e', 'b', 'f#', 'c#', 'g#', 'd#']
         self.keyList = self.keyListMajor + self.keyListMinor
 
-        self.keyLimit = 4
+        self.keyLimit = 5
         self.lengthChordCursor = 4
+
+        self._chordsPreDom = ['IV', 'iv', 'ii', 'II']
+
+        ##### Chord progressions (Major)
+        self.progsDominantMaj = [
+            ['V', 'I'],
+            ['V7', 'I'],
+            ['viio', 'I'],
+        ]
+
+        self.progsPreDomMaj = []
+        for prog in self.progsDominantMaj:
+            for chord in self._chordsPreDom:
+                newProg = [chord] + prog
+                self.progsPreDomMaj.append(newProg)
+
+        self.progsSpecificMaj = [
+            ['vi', 'ii', 'V'],
+            ['vi', 'ii', 'V7'],
+            ['vi', 'II', 'V'],
+            ['vi', 'II', 'V7'],
+        ]
+
+        self.chordsUnorderedMaj = [
+            ['I', 'IV', 'V']
+        ]
+
+        #### Chord progressions (Minor)
+        self.progsDominantMin = [
+            ['V', 'i'],
+            ['V7', 'i'],
+            ['viio', 'i'],
+        ]
+
+        self.progsPreDomMin = []
+        for prog in self.progsDominantMin:
+            for chord in self._chordsPreDom:
+                newProg = [chord] + prog
+                self.progsPreDomMin.append(newProg)
+
+        self.progsSpecificMin = [
+            ['bVI', 'iio', 'V'],
+            ['bVI', 'iio', 'V7'],
+            ['bVI', 'ii', 'V'],
+            ['bVI', 'ii', 'V7'],
+            ['bVI', 'II', 'V'],
+            ['bVI', 'II', 'V7'],
+        ]
+
+        self.chordsUnorderedMin = [
+            ['i', 'iv', 'v'],
+            ['i', 'iv', 'V'],
+        ]
 
     def findKeys(self, chordList):
         """
@@ -28,10 +80,15 @@ class KeyFinder:
         }
         Where the numbers represent where in the sequence each key starts.
         """
-        totalPossibleKeys = dict()
+        listPossibleKeys = []
 
         if len(chordList) <= self.lengthChordCursor:
-            return Utils.sortAndTrimDict(self._findPossibleKeys(chordList), self.keyLimit)
+            possibleKeys = sortAndTrimDict(self._findPossibleKeys(chordList), self.keyLimit)
+
+            for idx in range(len(chordList)):
+                listPossibleKeys.append(possibleKeys)
+
+            return listPossibleKeys
         else:
             maxIdx = len(chordList) - self.lengthChordCursor
 
@@ -40,9 +97,15 @@ class KeyFinder:
                     cursorEndIdx = idx + 4
                     chordListSlice = chordList[idx:cursorEndIdx]
                     cursorPossibleKeys = self._findPossibleKeys(chordListSlice)
-                    totalPossibleKeys = Utils.mergeDictionaries(totalPossibleKeys, cursorPossibleKeys)
+                    cursorPossibleKeys = sortAndTrimDict(cursorPossibleKeys, self.keyLimit)
 
-        return Utils.sortAndTrimDict(totalPossibleKeys, self.keyLimit)
+                    if(idx == 0):
+                        for startIndex in range(self.keyLimit):
+                            listPossibleKeys.append(cursorPossibleKeys)
+                    else:
+                        listPossibleKeys.append(cursorPossibleKeys)
+
+            return listPossibleKeys
 
     def _findPossibleKeys(self, chordList):
         """
@@ -69,13 +132,76 @@ class KeyFinder:
             avgChordFitScore = totalChordFitScore / len(chordList)
             possibleKeys[keyRoot] = avgChordFitScore
 
-        return Utils.sortAndTrimDict(possibleKeys, self.keyLimit)
+        return sortAndTrimDict(possibleKeys, self.keyLimit)
+
+
+    def _getRomanProgression(self, chordList, mKey):
+        """
+        Given a chordList and key, return a list of roman numeral chords.
+        """
+        chordListRoman = []
+
+        for chordSymbol in chordList:
+            formattedChordSymbol = convertToMusic21ChordSymbol(chordSymbol)
+            mChord = harmony.ChordSymbol(formattedChordSymbol)
+            romanObj = roman.romanNumeralFromChord(mChord, mKey)
+            chordListRoman.append(substituteRomanChords(romanObj.figure))
+
+        return chordListRoman
 
     def _detectProgressions(self, chordList, mKey):
         """
         Given a chordList and a key, determines if the chords fit the key in a better way.
         This is done through distinguishing chord movements like a ii-V-I.
-        Returns an integer, representing a score to be compared with other results.
+        Returns a float, representing a score to be compared with other results.
+        """
+        chordListRoman = self._getRomanProgression(chordList, mKey)
+
+        if mKey.mode == 'major':
+            return self._detectMajorProgressions(chordListRoman)
+        else:
+            return 1
+
+    def _detectMajorProgressions(self, romanChordList):
+        """
+        Detects important progressions in a major key.
+        Returns a float, represengint a score to be compared with other results.
+        """
+        running_score = 0
+
+        # if I, IV, and V are all present
+        for prog in self.chordsUnorderedMaj:
+            print('Comparing {0!s} and {1!s}'.format(prog, romanChordList))
+            if self._areChordsInProgression(prog, romanChordList):
+                running_score += 0.01
+        
+        return running_score
+
+    def _areChordsInProgression(self, searchChordList, chordList):
+        """
+        Determines if chords in searchChordList exist in the chordList.
+        Order doesn't matter. Returns a boolean.
+        """
+        if len(searchChordList) > len(chordList):
+            return False
+
+        chordsMatching = 0
+
+        for needleChord in searchChordList:
+            for haystackChord in chordList:
+                if needleChord == haystackChord:
+                    chordsMatching += 1
+
+        if chordsMatching == len(searchChordList):
+            return True
+        else:
+            return False
+
+    def _areExactChordsInProgression(self, searchChordList, chordList):
+        """
+        Determines if chords in searchChordList exist in the chordList.
+        Looks for the chords exactly the way they're listed.
+        Returns a boolean.
         """
         pass
 
@@ -84,7 +210,7 @@ class KeyFinder:
         Determines if the given chord fits in the given key.
         Returns a floating point number.
         """
-        formattedChordSymbol = Utils.convertToMusic21ChordSymbol(chordSymbol)
+        formattedChordSymbol = convertToMusic21ChordSymbol(chordSymbol)
         chordPitches = harmony.ChordSymbol(formattedChordSymbol).pitches
         keyPitches = mKey.pitches[:-1] # removing last note in scale!
         chordTonesInKey = 0
--- mchartanalyzer/keyfindertest.py
+++ mchartanalyzer/keyfindertest.py
@@ -12,6 +12,10 @@ class KeyFinderTest(unittest.TestCase):
         self.keyFinder = KeyFinder()
         self.pPrinter = pprint.PrettyPrinter(indent=2, width=120)
 
+        self.testChordsMajor1 = 'G Em C D'.split(' ')
+
+        self.testChordsMinor1 = 'Dm A Dm C F C Dm A7'.split(' ')
+
     def test_getChordFitScore(self):
         testChord1 = 'C'
         testKey1 = key.Key('C')
@@ -24,21 +28,76 @@ class KeyFinderTest(unittest.TestCase):
         self.assertTrue(result2 > 0.32 and result2 < 0.34) # We're checking if result2 is approximately 0.333
 
     def test_findPossibleKeys(self):
-        testChords = ['G', 'Em', 'C', 'D']
-        testResult = self.keyFinder._findPossibleKeys(testChords)
+        testResult = self.keyFinder._findPossibleKeys(self.testChordsMajor1)
 
         self.pPrinter.pprint('findPossibleKeys()')
         self.pPrinter.pprint(testResult)
 
+    def test_getRomanProgression(self):
+        testResult = self.keyFinder._getRomanProgression(self.testChordsMajor1, key.Key('G'))
+        expResult = ['I', 'vi', 'IV', 'V']
+        self.assertEqual(testResult, expResult)
+
+        testChords2 = 'G Em C D7'.split(' ')
+        testResult = self.keyFinder._getRomanProgression(testChords2, key.Key('G'))
+        expResult = ['I', 'vi', 'IV', 'V7']
+        self.assertEqual(testResult, expResult)
+
+        testChords3 = 'G Em D F#dim'.split(' ')
+        testResult = self.keyFinder._getRomanProgression(testChords3, key.Key('G'))
+        expResult = ['I', 'vi', 'V', 'viio']
+        self.assertEqual(testResult, expResult)
+
+        testChords4 = 'G Em D F#dim7'.split(' ')
+        testResult = self.keyFinder._getRomanProgression(testChords4, key.Key('G'))
+        expResult = ['I', 'vi', 'V', 'viio7']
+        self.assertEqual(testResult, expResult)    
+
+        testResult = self.keyFinder._getRomanProgression(testChords2, key.Key('C'))
+        expResult = ['V', 'iii', 'I', 'II7']
+        self.assertEqual(testResult, expResult)
+
+        testResult = self.keyFinder._getRomanProgression(self.testChordsMinor1, key.Key('d'))
+        expResult = ['i', 'V', 'i', 'bVII', 'III', 'bVII', 'i', 'V7']
+        self.assertEqual(testResult, expResult)
+
+    def test_detectProgressions(self):
+        testResult = self.keyFinder._detectProgressions(self.testChordsMajor1, key.Key('G'))
+        expResult = 0.01
+        self.assertEqual(testResult, expResult)
+
+    def test_detectMajorProgressions(self):
+        testList = ['I', 'iv', 'IV', 'V']
+        testResult = self.keyFinder._detectMajorProgressions(testList)
+        expResult = 0.01
+        self.assertEqual(testResult, expResult)
+
+    def test_areChordsInProgression(self):
+        testSearchList = ['I', 'IV', 'V']
+        testList = ['I', 'iv', 'IV', 'V']
+        testResult = self.keyFinder._areChordsInProgression(testSearchList, testList)
+        expResult = True
+        self.assertEqual(testResult, expResult)
+
+        testSearchList = ['I', 'IV', 'V']
+        testList = ['I', 'iv']
+        testResult = self.keyFinder._areChordsInProgression(testSearchList, testList)
+        expResult = False
+        self.assertEqual(testResult, expResult)
+
+        testSearchList = ['I', 'IV', 'V']
+        testList = ['I', 'iv', 'ii', 'V']
+        testResult = self.keyFinder._areChordsInProgression(testSearchList, testList)
+        expResult = False
+        self.assertEqual(testResult, expResult)
+
     def test_findKeys(self):
-        testChordsRaw1 = "G Em C D"
-        testChords1 = testChordsRaw1.split(" ")
         testChordsRaw2 = 'Bb Dm Eb Bb Bb Dm Eb Bb Bb Cm F Gm Dm Eb Bb Cm F Bb Bb Dm Eb Bb Bb Cm F Bb'
-        testChords2 = testChordsRaw2.split(" ")
+        testChords2 = testChordsRaw2.split(' ')
 
         self.pPrinter.pprint('findKeys()')
 
-        testResult1 = self.keyFinder.findKeys(testChords1)
+        testResult1 = self.keyFinder.findKeys(self.testChordsMajor1)
         testResult2 = self.keyFinder.findKeys(testChords2)
 
         self.pPrinter.pprint(testResult1)
